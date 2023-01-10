cca634d00ea3265622c2cf7624ee1c22e4d88a1e
atreiko <a.treiko92@gmail.com>
2023-01-10 17:57:00 +0200

    add home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,17 @@
+import { Header } from '@app/common/components/header/header.component';
+import { MenuList } from '@app//modules/menu/components/menu-list/menu-list.component';
+import pizzaMenu from '@app/mocks/pizza.json'
+import { Footer } from '@app/common/components/footer/footer.component';
+
 export const App = () => {
   return (
-    <h1>🍕 Pizza</h1>
+    <div>
+      <Header />
+      <div className='mx-12 mb-24'>
+        <MenuList items={pizzaMenu} />
+      </div>
+      <Footer />
+    </div>
   );
 }
 
