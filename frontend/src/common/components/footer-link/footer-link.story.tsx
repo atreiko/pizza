0da26f832ce6c08f1f2b4573989e4832197ec244
atreiko <a.treiko92@gmail.com>
2023-01-10 17:45:26 +0200

    add Footer

---
+++ frontend/src/common/components/footer-link/footer-link.story.tsx
@@ -0,0 +1,19 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { FooterLink } from '@app/common/components/footer-link/footer-link.component';
+
+export default {
+  title: 'Common/Footer link',
+  component: FooterLink,
+} as ComponentMeta<typeof FooterLink>;
+
+const Template: ComponentStory<typeof FooterLink> = (args) => (
+  <ul>
+    <FooterLink {...args} />
+  </ul>
+);
+
+export const View = Template.bind({});
+View.args = {
+  href: 'tel:+380441234567',
+  children: '044 123 45 67'
+}
